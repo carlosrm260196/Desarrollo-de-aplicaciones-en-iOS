c09bbab52bfe701330588715d6a53675a66a06f6
carlosrm260196 <36026123+carlosrm260196@users.noreply.github.com>
2018-02-01 00:10:16 -0600

    Add files via upload
    Actividad 1

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var v1=" esto es Swift"
+
+let c1=4
+
+var cadena:String=String()
+
+cadena="Hola mundo"+v1
+
+print("El contenido es: \(cadena)")
+
+print("El valor de constante es: \(c1)")
